4b411e709db25ea0d582fa50f229c5772c82579b
snshah55982 <shivnarayan2080.com>
2025-05-31 22:20:53 +0545

    changes

--- src/store/MainApi.js
+++ src/store/MainApi.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
+
+export const baseUrl = "https://reactecommercebackend.onrender.com";
 
 // ip of my laptop changes whenever i reconnect wifi network so i am using localhost for testing
 // export const baseUrl = "http://192.168.97.11:5000";
